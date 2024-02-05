03fe9e01987a7f282a738168636761fd3aad5251
jiangjiwei <jiangjiwei0322@outlook.com>
2024-02-05 15:38:46 +0800

    feat: 🎸 新增 dalle 重绘

--- unsplash.py
+++ unsplash.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2024/2/4 18:32
 # @Desc    :
-
+import json
+import logging
 import os
 import random
 import requests
@@ -10,9 +11,25 @@ import base64
 import PIL.Image
 import time
 
+from urllib3.connection import HTTPConnection
+
+# logging.basicConfig(level=logging.DEBUG)
+# # 是否打印 header
+# HTTPConnection.debuglevel = 1
+
 orientation_list = ["landscape", "portrait", "squarish"]
 
 GEMINI_KEY = "AIzaSyD24oWO1cWAn8keEpZCAe9fa6sdXIagd-I"
+TG_KEY = "sdfsf"
+
+
+def download_img_pure(url, file_name):
+    img_response = requests.get(url)
+    img_file_path = os.path.join(prepare_img_dir(), f"{file_name}.jpg")
+    with open(img_file_path, "wb") as img_file:
+        img_file.write(img_response.content)
+    img_file.close()
+    return img_file_path
 
 
 def prepare_img_dir():
@@ -29,6 +46,23 @@ def send_to_tg(img_file_path, tg_key, message, buttons):
         data['reply_markup'] = buttons
     headers = {'Authorization': f'{tg_key}'}
     response = requests.post(url, files=files, data=data, headers=headers)
+    if response.status_code != 200:
+        print(response.text)
+        raise Exception(f"Unexpected code {response.status_code}")
+
+
+def send_to_tg_media_group(img_file_url_list, tg_key, message, buttons):
+    if len(img_file_url_list) < 1:
+        return
+    url = f'https://tg-sender.flyooo.uk/sendMediaGroup?bot=AIImage'
+    media = []
+    for img_file_url in img_file_url_list:
+        media.append({'type': 'photo', 'media': img_file_url, 'caption': message, 'parse_mode': 'Markdown'})
+    data = {'media': media, 'chat_id': '799133753'}
+    if buttons:
+        data['reply_markup'] = buttons
+    headers = {'Authorization': f'{tg_key}'}
+    response = requests.post(url, json=data, headers=headers)
     if response.status_code != 200:
         print(response.text)
         raise Exception(f"Unexpected code {response.status_code}")
@@ -78,6 +112,8 @@ def get_image_desc_by_gemini(file_path):
 def get_random_image(gemini_key, unsplash_key, tg_key):
     global GEMINI_KEY
     GEMINI_KEY = gemini_key
+    global TG_KEY
+    TG_KEY = tg_key
     orientation = random.choice(orientation_list)
     url = f'https://api.unsplash.com/photos/random?client_id={unsplash_key}&count=1&orientation={orientation}'
     response = requests.get(url)
@@ -104,7 +140,20 @@ def get_random_image(gemini_key, unsplash_key, tg_key):
     if not prompt:
         prompt = get_image_desc_by_gemini(file_path_map['small'])
     sd_path = generate_image_by_sd(prompt)
-    send_to_tg(sd_path, 'monster.key', prompt, None)
+    send_to_tg(sd_path, TG_KEY, prompt, None)
+
+    dalle_draw_map = generate_image_by_dalle(prompt)
+    request_id = dalle_draw_map['requestId']
+    print("requestId: " + request_id)
+    # 休眠 30s，等待图片生成
+    print("waiting 30s......")
+    time.sleep(30)
+    imgs = query_dalle_result(request_id, prompt)
+    while len(imgs) == 0:
+        print("waiting 10s......")
+        time.sleep(10)
+        imgs = query_dalle_result(request_id, prompt)
+    send_to_tg_media_group(imgs, TG_KEY, "test", None)
 
 
 def download_img(img_id, img_urls):
@@ -150,8 +199,49 @@ def generate_image_by_sd(prompt):
     return file_path
 
 
-# if __name__ == '__main__':
-#     pwd = os.getcwd()
-#     prompt = get_image_desc_by_gemini(f"{pwd}/unsplash_img/2jRNVr0ac7s_small.jpg")
-#     sd_path = generate_image_by_sd(prompt)
-#     send_to_tg(sd_path, 'monster.key', prompt, None)
+def generate_image_by_dalle(prompt):
+    url = "https://ai-image-creator.jiwzdj.workers.dev/draw"
+
+    final_prompt = f"{prompt}."
+    print(f"finalPrompt: {final_prompt}\n")
+    data = {
+        'prompt': final_prompt,
+        'owner': 'monster',
+        'platform': 'Dall-E'
+    }
+    head = {
+        'x-token': '1uf0xfp%7C2%7Cfii%7C0%7C1478'
+    }
+    response = requests.post(url, json=data, headers=head)
+    if response.status_code != 200:
+        return ""
+    return response.json()
+
+
+def query_dalle_result(request_id, prompt):
+    url = "https://ai-image-creator.jiwzdj.workers.dev/queryDallEResult?requestId=" + request_id
+    head = {
+        'x-token': '1uf0xfp%7C2%7Cfii%7C0%7C1478'
+    }
+    response = requests.get(url, headers=head)
+    if response.status_code != 200:
+        return ""
+    text = response.text
+    if "In Progress" in text:
+        return []
+    print(text)
+    result_json = json.loads(text)
+    attempt_img = result_json['images']
+    # 判断 attempt_img 类型是否为 list,
+    # 如果是 list 类型，直接返回
+    if isinstance(attempt_img, list):
+        return attempt_img
+    # 如果不是 list 类型，尝试解析为 json
+    images = []
+    print("attempt_img text " + json.dumps(attempt_img))
+    print("attempt_img type " + str(type(attempt_img)))
+    try:
+        images = json.loads(attempt_img)
+    except Exception as e:
+        print(e)
+    return images
